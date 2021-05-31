d3a95bf0109c266db9dde30e335d41c9027912b9
Chufan Chen <allenplato28@gmail.com>
2021-05-31 15:33:01 +0800

    fix failed test: global variables are in a fixed loction in memory

--- codege.cpp
+++ codege.cpp
@@ -30,7 +30,13 @@ static int align_to(int n, int align) { return (n + align - 1) / align * align; 
 static void gen_addr(Node *node) {
   switch (node->kind) {
     case ND_VAR:
-      printf("  lea %d(%%rbp), %%rax\n", node->var->offset);
+      if (node->var->is_local) {
+        // Local variable
+        printf("  lea %d(%%rbp), %%rax\n", node->var->offset);
+      } else {
+        // Global variable
+        printf("  lea %s(%%rip), %%rax\n", node->var->name);
+      }
       return;
     case ND_DEREF:
       gen_expr(node->lhs);
@@ -203,9 +209,18 @@ static void assign_lvar_offsets(Obj *prog) {
   }
 }
 
-void codegen(Obj *prog) {
-  assign_lvar_offsets(prog);
+static void emit_data(Obj *prog) {
+  for (Obj *var = prog; var; var = var->next) {
+    if (var->is_function) continue;
 
+    printf("  .data\n");
+    printf("  .globl %s\n", var->name);
+    printf("%s:\n", var->name);
+    printf("  .zero %d\n", var->ty->size);
+  }
+}
+
+static void emit_text(Obj *prog) {
   for (Obj *fn = prog; fn; fn = fn->next) {
     if (!fn->is_function) continue;
 
@@ -233,4 +248,10 @@ void codegen(Obj *prog) {
     printf("  pop %%rbp\n");
     printf("  ret\n");
   }
+}
+
+void codegen(Obj *prog) {
+  assign_lvar_offsets(prog);
+  emit_data(prog);
+  emit_text(prog);
 }
