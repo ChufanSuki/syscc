cfc9ae990b68b39d7933e7f0595a88943bcf6f60
Chufan Chen <allenplato28@gmail.com>
2021-05-31 19:18:26 +0800

    Refactoring: Add a utility function

--- parse.cpp
+++ parse.cpp
@@ -104,9 +104,7 @@ static Obj *new_gvar(char *name, Type *ty) {
 
 static char *new_unique_name(void) {
   static int id = 0;
-  char *buf = (char*)calloc(1, 20);
-  sprintf(buf, ".L..%d", id++);
-  return buf;
+  return format(".L..%d", id++);
 }
 
 static Obj *new_anon_gvar(Type *ty) { return new_gvar(new_unique_name(), ty); }
